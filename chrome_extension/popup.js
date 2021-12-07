272cbf38acf61f84a218766c3360b52befe5a0ba
teresa622 <55215380+teresa622@users.noreply.github.com>
2021-12-06 21:56:05 -0600

    Update popup.js

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -13,10 +13,32 @@ changeColor.addEventListener("click", async () => {
   function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({ color }) => {
       x = document.getElementsByClassName("yt-simple-endpoint style-scope ytd-grid-video-renderer")
-      
+      links = []
       for (let element of x) {
-          element.style.backgroundColor = color
+        links.push(element.href)
       }
+      fetch('http://localhost:8080', {
+        headers: {'content-length': 100},
+        method: 'POST',
+        body: JSON.stringify(links)
+      }).then(r => r.text()).then(result => {
+        console.log(typeof result[0])
+        sentiments = (result.substring(1, result.length - 1)).split(",")
+        var nums = []
+        for (let i = 0; i < sentiments.length; i++) {
+          nums.push(parseFloat(sentiments[i], 10))
+        }
+        console.log(nums)
+      })
+      // for (let element of x) {
+      //     element.style.backgroundColor = color
+      // }
+      // var spawn = import("child_process");
+      // // const { spawn } = require('child_process');
+      // const childProcess = spawn('python', ['hello.py'])
+      // childProcess.stdout.on('data', (data) => {
+      //     console.log(`stdout: ${data}`)
+      // });
     //   document.body.style.backgroundColor = color;
     });
   }
